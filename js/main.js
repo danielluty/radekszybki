18a7a38c5f17630d7c87a002cac7c255bad01f9d
Daniel Luty <lutydaniel@gmail.com>
2019-07-31 00:22:55 +0200

    created single galleries

--- js/main.js
+++ js/main.js
@@ -1,9 +1,8 @@
 // Navigation Toggler
 (() => {
-
     const toggler = document.querySelector('.toggler'),
-          navMenu = document.querySelector('.nav--mobile'),
-          closeBtn = document.querySelector('.close-btn');
+        navMenu = document.querySelector('.nav--mobile'),
+        closeBtn = document.querySelector('.close-btn');
 
     toggler.addEventListener('click', () => {
         navMenu.classList.add('show-nav');
@@ -11,7 +10,47 @@
 
     closeBtn.addEventListener('click', () => {
         navMenu.classList.remove('show-nav');
-    })
-
+    });
 })();
 
+
+// Slider
+/* Show image when clicked */
+function openSlider() {
+    document.getElementById('image-slider').style.display = "inline-flex";
+    document.body.style.overflow = "hidden";
+}
+
+/* Close slider */
+function closeSlider() {
+    document.getElementById('image-slider').style.display = "none";
+    document.body.style.overflow = "auto";
+}
+
+let slideIndexJS = 1;
+
+showSlides(slideIndexJS);
+
+function changeSlides(n) {
+    showSlides(slideIndexJS += n);
+}
+
+function currentSlide(n) {
+    showSlides(slideIndexJS = n);
+}
+
+function showSlides(n) {
+    let i, slidesJS, slidesJSArr;
+    slidesJS = document.querySelectorAll('.slides');
+    slidesJSArr = Array.from(slidesJS);
+
+    if (n > slidesJS.length) slideIndexJS = 1;
+
+    if (n < 1) slideIndexJS = slidesJS.length;
+
+    for (i = 0; i < slidesJSArr.length; i++) {
+        slidesJSArr[i].style.display = "none";
+    }
+
+    slidesJSArr[slideIndexJS - 1].style.display = "block";
+}
